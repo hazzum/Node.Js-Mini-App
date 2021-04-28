fe942706b6c71cda312620051db5993b95b42514
Hazem <hazem.mix2012@gmail.com>
2021-04-29 01:04:00 +0200

    Final Build with error handling

--- node_server/index.js
+++ node_server/index.js
@@ -1,11 +1,11 @@
 const Joi = require('joi')
 const express = require('express');
+const path = require('path');
 const app = express();
+app.use(express.static(path.resolve(__dirname, './public')));
 var cors = require('cors')
 app.use(cors())
 app.use(express.json())
-app.use('/', express.static('./public'));
-
 ///////////////////////////////////////////////////////////////////////////////
 /////Schemas///////////////////////////////////////////////////////////////////
 const student_schema = Joi.object({
@@ -20,7 +20,7 @@ const course_schema = Joi.object({
 ///////////////////////////////////////////////////////////////////////////////
 ////DUMMY DATABASE/////////////////////////////////////////////////////////////
 let students = {
-    incrementor: 8,
+    incrementor: 6,
     list: [
         {
             name: 'Ahmed',
@@ -56,7 +56,7 @@ let students = {
 }
 ///////////////////////////////////////////////////////////////////////////////
 let courses = {
-    incrementor: 8,
+    incrementor: 4,
     list: [
         {
             name: 'Software Engineering',
@@ -210,5 +210,11 @@ app.delete('/api/students/:id', (req, res) => {
 });
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
+// All other GET requests not handled before will return our React app
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './public/', 'index.html'));
+  });
+
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Listening on port ${port}...`));
